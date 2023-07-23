0b9847740d38eb599f1c1bd19706602435998e1c
Galaxio <seongs1024@gmail.com>
2023-07-23 17:38:46 +0900

    feat: can parse multiple servers by vector iterator

--- dep/parser-test/src/main.cpp
+++ dep/parser-test/src/main.cpp
@@ -9,35 +9,36 @@
 
 int main() {
 	Parser parser;
-	parser.setFormat("listen", &Parser::parseIntTag);
-	parser.setFormat("host", &Parser::parseStringTag);
-	parser.setFormat("location", &ServerConfig::parseLocationTag);
+	parser.setFormat("server", &ServerConfig::parseServerBlock);
 	
 	std::ifstream file("default.conf");
-	ServerConfig serverConfig = (ServerConfig)parser.parse(file);
+	ServerConfig serverList = (ServerConfig)parser.parse(file);
 
-	int port = *serverConfig.at("listen").data;
-	std::string host = *serverConfig.at("host").data;
-
-
-	HashMap locationMap = *serverConfig.at("location").data;
-	KeyList locations = locationMap.getKeys();
-	HashMap pathLoc = *locationMap.at("/path").data;
-	HashMap path2Loc = *locationMap.at("/path2").data;
+	std::vector<ServerConfig> ServerConfigs = *serverList.at("server").data;
+	for (std::vector<ServerConfig>::const_iterator serverConfig = ServerConfigs.begin(); serverConfig != ServerConfigs.end(); ++serverConfig) {
+		int port = *serverConfig->at("listen").data;
+		std::string host = *serverConfig->at("host").data;
 
 
-	std::cout << "PORT: " << port << std::endl;
-	std::cout << "HOST: " << host << std::endl;
-	std::cout << "LOC: " << locations.size() << std::endl;
-	for (HashMap::const_iterator loc = locationMap.begin(); loc != locationMap.end(); ++loc) {
-		std::cout << "\tPATH: " << loc->first << std::endl;
-		HashMap path = *loc->second.data;
-		for (HashMap::const_iterator subTag = path.begin(); subTag != path.end(); ++subTag) {
-			std::vector<std::string> value = *subTag->second.data;
-			std::cout << "\t\tSUBKEY: " << subTag->first;
-			for (std::vector<std::string>::const_iterator method = value.begin(); method != value.end(); ++method)
-			 	std::cout << " " << *method;
-			std::cout << std::endl;
+		HashMap locationMap = *serverConfig->at("location").data;
+		KeyList locations = locationMap.getKeys();
+		HashMap pathLoc = *locationMap.at("/path").data;
+		HashMap path2Loc = *locationMap.at("/path2").data;
+
+
+		std::cout << "PORT: " << port << std::endl;
+		std::cout << "HOST: " << host << std::endl;
+		std::cout << "LOC: " << locations.size() << std::endl;
+		for (HashMap::const_iterator loc = locationMap.begin(); loc != locationMap.end(); ++loc) {
+			std::cout << "\tPATH: " << loc->first << std::endl;
+			HashMap path = *loc->second.data;
+			for (HashMap::const_iterator subTag = path.begin(); subTag != path.end(); ++subTag) {
+				std::vector<std::string> value = *subTag->second.data;
+				std::cout << "\t\tSUBKEY: " << subTag->first;
+				for (std::vector<std::string>::const_iterator method = value.begin(); method != value.end(); ++method)
+					std::cout << " " << *method;
+				std::cout << std::endl;
+			}
 		}
 	}
 }
--- dep/server-config/inc/ServerConfig.hpp
+++ dep/server-config/inc/ServerConfig.hpp
@@ -4,9 +4,7 @@
 #include <string>
 
 class ServerConfig: public Tags {
-public:
-    ServerConfig(const Tags &other): Tags(other) {};
-
+private:
     static void parseLocationTag(std::istream &stream, const std::string *key, Tags &tags) {
         std::string path;
         stream >> path;
@@ -25,5 +23,30 @@ public:
         locationParser.setFormat("allowed_methods", &Parser::parseVecOfStringTag);
 
         locations.insert(std::make_pair(path, UniquePtr<Any>(AnyType<Tags>(locationParser.parse(stream)))));
+    }
+
+public:
+    ServerConfig(const Tags &other): Tags(other) {};
+
+    static void parseServerBlock(std::istream &stream, const std::string *key, Tags &tags) {
+        std::string server;
+        stream >> server;
+        if (stream.fail() || server.empty()) {
+            stream.clear();
+            throw std::runtime_error("parse error: " + *key);
+        }
+        Parser::skipLineEnd(stream);
+
+        UniquePtr<Any> serverGroup = UniquePtr<Any>(AnyType<std::vector<ServerConfig> >(std::vector<ServerConfig>()));
+        std::pair<Tags::iterator, bool> insertion = tags.insert(std::make_pair(*key, serverGroup));
+
+        std::vector<ServerConfig> &servers = (std::vector<ServerConfig> &)*insertion.first->second.data;
+
+        Parser serverParser;
+        serverParser.setFormat("listen", &Parser::parseIntTag);
+	    serverParser.setFormat("host", &Parser::parseStringTag);
+	    serverParser.setFormat("location", &ServerConfig::parseLocationTag);
+
+        servers.push_back(serverParser.parse(stream));
     }
 };
