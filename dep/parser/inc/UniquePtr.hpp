7e2b772976f3818609471458acffe77e1f6de3bf
Galaxio <seongs1024@gmail.com>
2023-07-23 17:06:51 +0900

    fix: add ref_count to fix double free of data

--- dep/parser/inc/UniquePtr.hpp
+++ dep/parser/inc/UniquePtr.hpp
@@ -4,18 +4,27 @@ template <class T>
 class UniquePtr {
 public:
     T *data;
+    int *ref_count;
 
+    UniquePtr(const UniquePtr &rhs);
     template <class U> UniquePtr(const U &rhs);
     ~UniquePtr();
 };
 
-template <class T> template <class U>
-UniquePtr<T>::UniquePtr(const U &rhs): data(new U(rhs)) {
+template <class T>
+UniquePtr<T>::UniquePtr(const UniquePtr<T> &rhs): data(rhs.data), ref_count(rhs.ref_count) {
+    ++(*this->ref_count);
+}
 
+template <class T> template <class U>
+UniquePtr<T>::UniquePtr(const U &rhs): data(new U(rhs)), ref_count(new int(1)) {
 }
 
 template <class T>
 UniquePtr<T>::~UniquePtr() {
-    if (this->data)
+    --(*this->ref_count);
+    if (this->data && this->ref_count && *this->ref_count == 0) {
         delete this->data;
+        delete this->ref_count;
+    }
 }
